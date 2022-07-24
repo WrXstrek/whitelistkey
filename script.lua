0d1baa662bcb4aae266fe26e424a5d71adff25b0
WrXstrek <101234937+WrXstrek@users.noreply.github.com>
2022-07-24 15:46:56 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,7 +1,7 @@
 s = _G.Key or ""
-if game:HttpGet("https://WEBSITENAME.heroku.com/bot.js?key="..s) == "Whitelisted!" then
+if game:HttpGet("https://lolst.heroku.com/bot.js?key="..s) == "Whitelisted!" then
 print("Whitelisted")
-elseif game:HttpGet("https://WEBSITENAME.heroku.com/bot.js?key="..s) == "Not Whitelisted"
+elseif game:HttpGet("https://lolst.heroku.com/bot.js?key="..s) == "Not Whitelisted"
 game.Players.LocalPlayer:Kick("Not Whitelisted")
 else
 game.Players.LocalPlayer:Kick("Not Whitelisted")
